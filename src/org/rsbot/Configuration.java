bf9f70d5f6861aa08b43c9eee921638af4647416
(no author) <(no author)@f5c96de0-3062-e754-a605-3a43412c0151>
2011-09-09 20:43:55 +0000

    Updated SITE

--- src/org/rsbot/Configuration.java
+++ src/org/rsbot/Configuration.java
@@ -78,7 +78,7 @@ public class Configuration {
 			public static final String CLIENTPATCH = HOST + "modscript.gz";
 			public static final String VERSION = HOST + "version.txt";
 			public static final String PROJECT = "http://code.google.com/p/skbot-client/";
-			public static final String SITE = "http://skproductions.tk";
+			public static final String SITE = "http://SKBot.tk";
 			public static final String SDN_MANIFEST = HOST + "sdn-manifest.txt";
 			public static final String SDN_USER = HOST  + "sdn-user";
 			public static final String AD_INFO = HOST + "botad-info.txt";
